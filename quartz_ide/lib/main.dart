28100f923d1bc4fa7cab73eb1a4d6387705a2e00
Bachuchee <gal.aloni@outlook.com>
2023-03-28 19:44:33 +0300

    Add Theme

--- quartz_ide/lib/main.dart
+++ quartz_ide/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quartz_ide/theme/color_scheme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,20 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quartz IDE',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: darkColorScheme,
+        fontFamily: 'Sen',
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Quartz IDE'),
     );
   }
 }
